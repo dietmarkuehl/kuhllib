89e3555f0d3583e2388383b8b210ab1fd1057903
Dietmar Kühl <dietmar.kuehl@gmail.com>
2014-12-14 22:48:32 +0000

    model_value to kick projections off

---
+++ src/nstd/projection/model_value.t.cpp
@@ -0,0 +1,50 @@
+// nstd/projection/model_value.t.cpp                                  -*-C++-*-
+// ----------------------------------------------------------------------------
+//  Copyright (C) 2014 Dietmar Kuehl http://www.dietmar-kuehl.de         
+//                                                                       
+//  Permission is hereby granted, free of charge, to any person          
+//  obtaining a copy of this software and associated documentation       
+//  files (the "Software"), to deal in the Software without restriction, 
+//  including without limitation the rights to use, copy, modify,        
+//  merge, publish, distribute, sublicense, and/or sell copies of        
+//  the Software, and to permit persons to whom the Software is          
+//  furnished to do so, subject to the following conditions:             
+//                                                                       
+//  The above copyright notice and this permission notice shall be       
+//  included in all copies or substantial portions of the Software.      
+//                                                                       
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND             
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT          
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,         
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING         
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR        
+//  OTHER DEALINGS IN THE SOFTWARE. 
+// ----------------------------------------------------------------------------
+
+#include "nstd/projection/model_value.hpp"
+#include "kuhl/test.hpp"
+
+namespace NP = nstd::projection;
+namespace KT = kuhl::test;
+
+// ----------------------------------------------------------------------------
+
+static KT::testcase const tests[] = {
+    KT::expect_success("simple construction", [](KT::context& c)->bool{
+            return assert_equal(c, "explicit construction", NP::model_value<int>('a').get_value(), 'a')
+                ;
+        }),
+    KT::expect_success("assert type of get_value()", [](KT::context& c)->bool{
+            NP::model_value<int> value(10);
+            return assert_equal(c, "get_value()", value.get_value(), 10)
+                && KT::assert_type<int const&, decltype(value.get_value())>(c, "type");
+                ;
+        }),
+};
+
+int main(int ac, char* av[])
+{
+    return KT::run_tests("property::model_value", ac, av, ::tests);
+}
