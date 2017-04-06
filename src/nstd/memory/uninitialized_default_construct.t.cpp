ad9aef31b91bd7b5f71a78792ac55fbe5256a627
Dietmar Kühl <dietmar.kuehl@me.com>
2017-04-06 19:23:05 +0100

    added a class template to restrict iterator capabilities

--- src/nstd/memory/uninitialized_default_construct.t.cpp
+++ src/nstd/memory/uninitialized_default_construct.t.cpp
@@ -24,15 +24,73 @@
 // ----------------------------------------------------------------------------
 
 #include "nstd/memory/uninitialized_default_construct.hpp"
+#include "nstd/execution/execution.hpp"
+#include "nstd/atomic/atomic.hpp"
 #include "kuhl/test.hpp"
 
 namespace KT = ::kuhl::test;
+namespace NM = ::nstd::memory;
+namespace NA = ::nstd::atomic;
+
+// ----------------------------------------------------------------------------
+
+namespace {
+    struct pod {
+        int value;
+    };
+    struct counting {
+        static NA::atomic<int> count;
+        int value;
+        counting(): value(17) { ++count; }
+    };
+    NA::atomic<int> counting::count;
+}
 
 // ----------------------------------------------------------------------------
 
 static KT::testcase const tests[] = {
-    KT::expect_failure("placeholder", [](KT::context& c)->bool{
-           return false;
+    KT::expect_success("plain: initializing an empty sequence does nothing",
+                       [](KT::context& c)->bool {
+        counting array[1] = {};
+        array[0].value = 18;
+        NM::uninitialized_default_construct(array + 0, array + 0);
+        return KT::assert_equal(c, "count of constructions", counting::count, 1)
+            && KT::assert_equal(c, "value is untouched", array[0].value, 18)
+            ;
+        }),
+    KT::expect_success("plain: a sequence of PODs isn't changed them",
+                       [](KT::context& c)->bool {
+        pod array[3] = {};
+        for (int i(0); i != 3; ++i) {
+            array[i].value = i;
+        }
+
+        return KT::assert_equal(c, "array[0].value == 0", array[0].value, 0)
+            && KT::assert_equal(c, "array[1].value == 1", array[1].value, 1)
+            && KT::assert_equal(c, "array[2].value == 2", array[2].value, 2)
+            && (NM::uninitialized_default_construct(array + 0, array + 3), true)
+            && KT::assert_equal(c, "array[0].value == 0", array[0].value, 0)
+            && KT::assert_equal(c, "array[1].value == 1", array[1].value, 1)
+            && KT::assert_equal(c, "array[2].value == 2", array[2].value, 2)
+            ;
+        }),
+    KT::expect_success("plain: a sequence of non-PODs is changed",
+                       [](KT::context& c)->bool {
+        counting::count = 0;
+        counting array[3] = {};
+        for (int i(0); i != 3; ++i) {
+            array[i].value = i;
+        }
+        return KT::assert_equal(c, "counting::count == 3", counting::count, 3)
+            && KT::assert_equal(c, "array[0].value == 0", array[0].value, 0)
+            && KT::assert_equal(c, "array[1].value == 1", array[1].value, 1)
+            && KT::assert_equal(c, "array[2].value == 2", array[2].value, 2)
+            && (NM::uninitialized_default_construct(array + 0, array + 3), true)
+            && KT::assert_equal(c, "counting::count == 6", counting::count, 6)
+            && KT::assert_equal(c, "array[0].value == 17", array[0].value, 17)
+            && KT::assert_equal(c, "array[1].value == 17", array[1].value, 17)
+            && KT::assert_equal(c, "array[2].value == 17", array[2].value, 17)
+            ;
         }),
 };
 
