3c263d8c381ec4c13aa6c8bcb0af83a1d266d19f
Dietmar Kühl <dietmar.kuehl@me.com>
2021-09-25 15:19:47 +0000

    added an algorithm to run a context until a sender is done

---
+++ src/nstd/execution/run.hpp
@@ -0,0 +1,83 @@
+// nstd/execution/run.hpp                                             -*-C++-*-
+// ----------------------------------------------------------------------------
+//  Copyright (C) 2021 Dietmar Kuehl http://www.dietmar-kuehl.de         
+//                                                                       
+//  Permission is hereby granted, free of charge, to any person          
+//  obtaining a copy of this software and associated documentation       
+//  files (the "Software"), to deal in the Software without restriction, 
+//  including without limitation the rights to use, copy, modify,        
+//  merge, publish, distribute, sublicense, and/or sell copies of        
+//  the Software, and to permit persons to whom the Software is          
+//  furnished to do so, subject to the following conditions:             
+//                                                                       
+//  The above copyright notice and this permission notice shall be       
+//  included in all copies or substantial portions of the Software.      
+//                                                                       
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND             
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT          
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,         
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING         
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR        
+//  OTHER DEALINGS IN THE SOFTWARE. 
+// ----------------------------------------------------------------------------
+
+#ifndef INCLUDED_NSTD_EXECUTION_RUN
+#define INCLUDED_NSTD_EXECUTION_RUN
+
+#include "nstd/execution/sender.hpp"
+#include "nstd/execution/connect.hpp"
+#include "nstd/execution/set_value.hpp"
+#include "nstd/execution/set_error.hpp"
+#include "nstd/execution/set_done.hpp"
+#include "nstd/execution/start.hpp"
+#include "nstd/utility/forward.hpp"
+#include <atomic>
+
+// ----------------------------------------------------------------------------
+
+namespace nstd::hidden_names
+{
+    struct run_receiver
+    {
+        ::std::atomic<bool>* done;
+        template <typename... Args>
+        friend auto tag_invoke(::nstd::execution::set_value_t, run_receiver&& r, Args&&...)
+            noexcept -> void
+        {
+            *r.done = true;
+        }
+        template <typename Error>
+        friend auto tag_invoke(::nstd::execution::set_error_t, run_receiver&& r, Error&&)
+            noexcept -> void
+        {
+            *r.done = true;
+        }
+        friend auto tag_invoke(::nstd::execution::set_done_t, run_receiver&& r)
+            noexcept -> void
+        {
+            *r.done = true;
+        }
+    };
+}
+
+namespace nstd::execution {
+    template<typename Context, ::nstd::execution::sender Sender>
+    auto run(Context& context, Sender&& sender)
+    {
+        ::std::atomic<bool> done(false);
+        auto state(::nstd::execution::connect(::nstd::utility::forward<Sender>(sender),
+                                              ::nstd::hidden_names::run_receiver{&done}));
+        ::nstd::execution::start(state);
+        while (!done  && 1u == context.run_one()) {
+        }
+        if (!done) {
+            //-dk:TODO need to cancel the operation before destroying the state
+        }
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+#endif
---
+++ src/nstd/execution/run.t.cpp
@@ -0,0 +1,116 @@
+// nstd/execution/run.t.cpp                                           -*-C++-*-
+// ----------------------------------------------------------------------------
+//  Copyright (C) 2021 Dietmar Kuehl http://www.dietmar-kuehl.de         
+//                                                                       
+//  Permission is hereby granted, free of charge, to any person          
+//  obtaining a copy of this software and associated documentation       
+//  files (the "Software"), to deal in the Software without restriction, 
+//  including without limitation the rights to use, copy, modify,        
+//  merge, publish, distribute, sublicense, and/or sell copies of        
+//  the Software, and to permit persons to whom the Software is          
+//  furnished to do so, subject to the following conditions:             
+//                                                                       
+//  The above copyright notice and this permission notice shall be       
+//  included in all copies or substantial portions of the Software.      
+//                                                                       
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES      
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND             
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT          
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,         
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING         
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR        
+//  OTHER DEALINGS IN THE SOFTWARE. 
+// ----------------------------------------------------------------------------
+
+#include "nstd/execution/run.hpp"
+#include "nstd/execution/just.hpp"
+#include "nstd/type_traits/remove_cvref.hpp"
+#include "nstd/utility/move.hpp"
+#include "kuhl/test.hpp"
+#include <deque>
+#include <functional>
+
+namespace test_declarations {}
+namespace TD = ::test_declarations;
+namespace KT = ::kuhl::test;
+namespace EX = ::nstd::execution;
+namespace TT = ::nstd::type_traits;
+namespace UT = ::nstd::utility;
+
+// ----------------------------------------------------------------------------
+
+namespace test_declarations {
+    namespace {
+        class context
+        {
+        private:
+            ::std::deque<::std::function<auto()->void>> d_jobs;
+            ::std::size_t                               d_run{};
+        
+        public:
+            auto run_one() -> ::std::size_t {
+                if (this->d_jobs.empty()) {
+                    return 0u;
+                }
+                this->d_jobs.front()();
+                this->d_jobs.pop_front();
+                ++this->d_run;
+                return 1u;
+            }
+            auto count() const -> ::std::size_t { return this->d_run; }
+            auto submit(::std::function<auto()->void> job) {
+                this->d_jobs.emplace_back(::nstd::utility::move(job));
+            }
+        };
+
+        class sender
+        {
+        public:
+            template <template <typename...> class V, template <typename...> class T>
+            using value_types = V<T<>>;
+            template <template <typename...> class V>
+            using error_types = V<>;
+            static constexpr bool sends_done = false;
+        
+            TD::context* d_context;
+
+            template <typename Receiver>
+            struct state {
+                TT::remove_cvref_t<Receiver> d_receiver;
+                TD::context*                 d_context;
+                friend auto tag_invoke(EX::start_t, state& s)
+                    noexcept -> void
+                {
+                    s.d_context->submit([&receiver = s.d_receiver]{
+                        EX::set_value(UT::move(receiver));
+                    });
+                }
+            };
+            template <typename Receiver>
+            friend auto tag_invoke(EX::connect_t, sender s, Receiver&& r) -> state<Receiver>
+            {
+                return state<Receiver>{ UT::forward<Receiver>(r), s.d_context };
+            }
+        };
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+static KT::testcase const tests[] = {
+    KT::expect_success("sender already complete when run", []{
+            TD::context context;
+            EX::run(context, EX::just());
+            return 0u == context.count();
+                ;
+        }),
+    KT::expect_success("sender completes upon context run", []{
+            TD::context context;
+            EX::run(context, TD::sender{&context});
+            return 1u == context.count();
+                ;
+        }),
+};
+
+static KT::add_tests suite("run", ::tests);
