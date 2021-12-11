b5cd5d9b75406b5b0be143e0dcd1a0ee11076201
Dietmar Kuehl <dietmar.kuehl@me.com>
2021-12-11 21:10:38 +0000

    added result reporting to the observer context

--- src/nstd/file/observer_context.cpp
+++ src/nstd/file/observer_context.cpp
@@ -24,12 +24,37 @@
 // ----------------------------------------------------------------------------
 
 #include <nstd/file/observer_context.hpp>
+#include <nstd/utility/move.hpp>
 #include <iostream>
 
 // ----------------------------------------------------------------------------
 
 namespace nstd::file {
     int observer_context_dummy = 0;
+}
+
+// ----------------------------------------------------------------------------
+
+namespace
+{
+    template <typename Fun>
+    struct continuation
+        : ::nstd::file::context::io_base
+    {
+        ::nstd::file::context::io_base* d_cont;
+        Fun                             d_fun;
+        continuation(::nstd::file::context::io_base* cont,
+                     Fun&& fun)
+            : d_cont(cont)
+            , d_fun(::nstd::utility::move(fun)) {
+        }
+        auto do_result(::std::int32_t rc, ::std::uint32_t flags) -> void override {
+            ::nstd::file::context::io_base* cont(this->d_cont);
+            this->d_fun(rc, flags);
+            delete this;
+            cont->result(rc, flags);
+        }
+    };
 }
 
 // ----------------------------------------------------------------------------
@@ -43,54 +68,72 @@ nstd::file::observer_context::observer_context(::nstd::file::context& context)
 
 auto nstd::file::observer_context::do_run_one() -> count_type
 {
-    ::std::cout << "<> run_one()\n";
-    return this->d_context.run_one();
+    ::std::cout << "> run_one()\n";
+    count_type rc(this->d_context.run_one());
+    ::std::cout << "< run_one(): rc=" << rc << "\n";
+    return rc;
 }
 
 auto nstd::file::observer_context::do_nop(io_base* cont) -> void
 {
-    ::std::cout << "<> nop()\n";
-    this->d_context.nop(cont);
+    ::std::cout << "> nop()\n";
+    this->d_context.nop(new continuation(cont, [](int rc, int flags){
+        ::std::cout << "< nop: rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
 
 auto nstd::file::observer_context::do_timer(time_spec* spec, io_base* cont) -> void
 {
-    ::std::cout << "<> timer()\n";
-    this->d_context.timer(spec, cont);
+    ::std::cout << "> timer()\n";
+    this->d_context.timer(spec, new continuation(cont, [](int rc, int flags){
+        ::std::cout << "< timer: rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
 
 auto nstd::file::observer_context::do_accept(native_handle_type fd, ::sockaddr* addr, ::socklen_t* len, int flags, io_base* cont) -> void
 {
-    ::std::cout << "<> accept(" << fd << ", ...)\n";
-    this->d_context.accept(fd, addr, len, flags, cont);
+    ::std::cout << "> accept(" << fd << ", ...)\n";
+    this->d_context.accept(fd, addr, len, flags, new continuation(cont, [fd](int rc, int flags){
+        ::std::cout << "< accept(" << fd << "): rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
 
 auto nstd::file::observer_context::do_connect(native_handle_type fd, ::sockaddr const* addr, ::socklen_t len, io_base* cont) -> void
 {
-    ::std::cout << "<> connect(" << fd << ", ...)\n";
-    this->d_context.connect(fd, addr, len, cont);
+    ::std::cout << "> connect(" << fd << ", ...)\n";
+    this->d_context.connect(fd, addr, len, new continuation(cont, [fd](int rc, int flags){
+        ::std::cout << "< connect(" << fd << "): rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
 
 auto nstd::file::observer_context::do_sendmsg(native_handle_type fd, ::msghdr const* hdr, int len, io_base* cont) -> void
 {
-    ::std::cout << "<> sendmsg(" << fd << ", ...)\n";
-    this->d_context.sendmsg(fd, hdr, len, cont);
+    ::std::cout << "> sendmsg(" << fd << ", ...)\n";
+    this->d_context.sendmsg(fd, hdr, len, new continuation(cont, [fd](int rc, int flags){
+        ::std::cout << "< sendmsg(" << fd << "): rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
 
 auto nstd::file::observer_context::do_recvmsg(native_handle_type fd, ::msghdr* hdr, int len, io_base* cont) -> void
 {
-    ::std::cout << "<> recvmsg(" << fd << ", ...)\n";
-    this->d_context.recvmsg(fd, hdr, len, cont);
+    ::std::cout << "> recvmsg(" << fd << ", ...)\n";
+    this->d_context.recvmsg(fd, hdr, len, new continuation(cont, [fd](int rc, int flags){
+        ::std::cout << "< recvmsg(" << fd << "): rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
 
 auto nstd::file::observer_context::do_read(int fd, ::iovec* vec, ::std::size_t len, io_base* cont) -> void
 {
-    ::std::cout << "<> read(" << fd << ", ...)\n";
-    this->d_context.read(fd, vec, len, cont);
+    ::std::cout << "> read(" << fd << ", ...)\n";
+    this->d_context.read(fd, vec, len, new continuation(cont, [fd](int rc, int flags){
+        ::std::cout << "< read(" << fd << "): rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
 
 auto nstd::file::observer_context::do_open_at(int fd, char const* path, int flags, io_base* cont) -> void
 {
-    ::std::cout << "<> open_at(" << fd << ", '" << path << "', " << std::hex << flags << std::dec << ")\n";
-    this->d_context.open_at(fd, path, flags, cont);
+    ::std::cout << "> open_at(" << fd << ", '" << path << "', " << std::hex << flags << std::dec << ")\n";
+    this->d_context.open_at(fd, path, flags, new continuation(cont, [fd](int rc, int flags){
+        ::std::cout << "< open_at(" << fd << "): rc=" << rc << ", flags=" << flags << "\n";
+    }));
 }
