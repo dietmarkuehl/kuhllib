a3d34e8e5ffe4435a6f9d6d7fabfc00e0b8b3a2a
Dietmar Kuehl <dietmar.kuehl@me.com>
2022-01-18 00:33:08 +0000

    added a mock up of how audio support might look like

--- src/examples/audio.cpp
+++ src/examples/audio.cpp
@@ -28,8 +28,12 @@
 
 #include "nstd/execution.hpp"
 #include "nstd/thread/sync_wait.hpp"
+#include <atomic>
+#include <functional>
 #include <iostream>
+#include <memory>
 #include <span>
+#include <thread>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -41,92 +45,165 @@ namespace EX = ::nstd::execution;
 
 namespace audio
 {
+    class context
+    {
+    public:
+        struct start_desc {};
+        struct end_desc {};
+
+    private:
+        ::std::vector<::std::byte>                            d_buffer{10u};
+        ::std::function<auto(start_desc const&)->void>        d_start;
+        ::std::function<auto(::std::span<::std::byte>)->void> d_process;
+        ::std::function<auto(end_desc const&)->void>          d_end;
+        ::std::unique_ptr<::std::jthread>                     d_thread;
+
+    public:
+        template <typename Start, typename Process, typename End>
+        auto start(Start&& s, Process&& p, End&& e) ->void {
+            this->d_start   = ::std::forward<Start>(s);
+            this->d_process = ::std::forward<Process>(p);
+            this->d_end     = ::std::forward<End>(e);
+            this->d_thread = ::std::make_unique<::std::jthread>([this]{ this->run(3u); });
+        }
+
+        auto run(::std::size_t count) -> void {
+            this->d_start(start_desc());
+            for (::std::size_t i{0u}; i != count; ++i) {
+                this->d_process(this->d_buffer);
+            }
+            this->d_end(end_desc());
+        }
+    };
+
+    class buffer_processor {
+    protected:
+        virtual auto do_process(::std::span<::std::byte>) -> void = 0;
+    public:
+        virtual ~buffer_processor() {}
+        auto process(::std::span<::std::byte> buffer) -> void {
+            this->do_process(buffer);
+        }
+    };
+
     template <EX::receiver Receiver>
-    struct buffer_state {
-        ::std::remove_cvref_t<Receiver> d_receiver;
-        ::std::span<std::byte>          d_buffer;
+    struct buffer_state
+        : buffer_processor
+    {
+        ::std::remove_cvref_t<Receiver>            d_receiver;
+        ::std::atomic<::audio::buffer_processor*>* d_hook;
+
+        template <EX::receiver R>
+        buffer_state(R&& r, ::std::atomic<::audio::buffer_processor*>* hook)
+            : d_receiver(::std::forward<R>(r))
+            , d_hook(hook)
+        {
+        }
 
         friend auto tag_invoke(EX::start_t, buffer_state& self) noexcept -> void {
-            if (self.d_buffer.empty()) {
-                EX::set_done(::std::move(self.d_receiver));
+            *self.d_hook = &self;
+        }
+
+        auto do_process(::std::span<::std::byte> buffer) -> void override {
+            if (buffer.empty()) {
+                EX::set_done(::std::move(this->d_receiver));
             }
             else {
-                EX::set_value(::std::move(self.d_receiver), self.d_buffer);
+                EX::set_value(::std::move(this->d_receiver), buffer);
             }
         }
     };
 
-    struct buffer_sender {
+    struct buffer_sender
+    {
         template <template <typename...> class T, template <typename...> class V>
         using value_types = V<T<::std::span<::std::byte>>>;
         template <template <typename...> class V>
         using error_types = V<::std::exception_ptr>;
         static constexpr bool sends_done = false;
 
-        mutable ::std::size_t               d_count;
-        mutable ::std::vector<::std::byte>  d_buffer;
+        ::std::atomic<::audio::buffer_processor*>* d_hook;
 
-        buffer_sender(::std::size_t count): d_count(count), d_buffer(10) {}
+        buffer_sender(::std::atomic<::audio::buffer_processor*>* hook): d_hook(hook) {}
 
         template <EX::receiver Receiver>
         friend auto tag_invoke(EX::connect_t, buffer_sender const& self, Receiver&& receiver) noexcept
             -> audio::buffer_state<Receiver> {
-            return audio::buffer_state<Receiver>{
-                ::std::forward<Receiver>(receiver),
-                self.d_count--? ::std::span(self.d_buffer): ::std::span(self.d_buffer.begin(), self.d_buffer.begin())
-                };
+            return audio::buffer_state<Receiver>(::std::forward<Receiver>(receiver), self.d_hook);
         }
     };
 
     struct start_description {
-        std::size_t d_count;
+        ::std::atomic<::audio::buffer_processor*>* d_hook;
         auto process() const -> audio::buffer_sender {
-            return audio::buffer_sender(this->d_count);
+            return audio::buffer_sender(this->d_hook);
         }
     };
 
     template <EX::receiver Receiver>
     struct state
     {
-        ::std::remove_cvref_t<Receiver> d_receiver;
-        audio::start_description       d_start_description;
+        ::std::remove_cvref_t<Receiver>           d_receiver;
+        audio::context*                           d_context;
+        ::std::atomic<::audio::buffer_processor*> d_hook;
         friend auto tag_invoke(EX::start_t, state& self) noexcept -> void {
-            EX::set_value(::std::move(self.d_receiver), self.d_start_description);
+            self.d_context->start(
+                [&self](::audio::context::start_desc){
+                    EX::set_value(::std::move(self.d_receiver), start_description{ &self.d_hook });
+                },
+                [&self](::std::span<::std::byte> buffer) {
+                    // === problem ===
+                    while (self.d_hook == nullptr) {
+                    }
+                    audio::buffer_processor* hook = self.d_hook;
+                    self.d_hook = nullptr;
+                    ::std::cout << "need to fill in bytes\n";
+                    hook->process(buffer);
+                },
+                [&self](::audio::context::end_desc){
+                    while (self.d_hook == nullptr) {
+                    }
+                    audio::buffer_processor* hook = self.d_hook;
+                    self.d_hook = nullptr;
+                    static ::std::byte bytes[1];
+                    hook->process(::std::span<::std::byte>(+bytes, 0u));
+                }
+            );
         }
     };
 
     struct sender
     {
         template <template <typename...> class T, template <typename...> class V>
-        using value_types = V<T<audio::start_description&>>;
+        using value_types = V<T<audio::start_description>>;
         template <template <typename...> class V>
         using error_types = V<std::exception_ptr>;
         static constexpr bool sends_done = false;
 
-        std::size_t d_count;
+        ::audio::context* d_context;
 
         template <EX::receiver Receiver>
         friend auto tag_invoke(EX::connect_t, sender const& self, Receiver&& receiver) -> state<Receiver>
         {
-            return state<Receiver>{ ::std::forward<Receiver>(receiver), { self.d_count } };
+            return state<Receiver>{ ::std::forward<Receiver>(receiver), self.d_context, {} };
         }
         template <EX::receiver Receiver>
         friend auto tag_invoke(EX::connect_t, sender&& self, Receiver&& receiver) -> state<Receiver>
         {
-            return state<Receiver>{ ::std::forward<Receiver>(receiver), { self.d_count } };
+            return state<Receiver>{ ::std::forward<Receiver>(receiver), self.d_context, {} };
         }
     };
 
     struct scheduler
     {
-        ::std::size_t d_count;
+        ::audio::context* d_context;
         friend auto tag_invoke(EX::schedule_t, scheduler const& self) noexcept -> audio::sender {
-            return audio::sender{ self.d_count };
+            return audio::sender{ self.d_context };
         }
     };
 
-    auto get_configured_audio_device(::std::size_t count) -> scheduler {
-        return audio::scheduler{count};
+    auto get_configured_audio_device(::audio::context& context) -> scheduler {
+        return audio::scheduler{&context};
     }
 }
 
@@ -134,7 +211,8 @@ namespace audio
 
 int main()
 {
-    auto s = audio::get_configured_audio_device(3);
+    audio::context context;
+    auto s = audio::get_configured_audio_device(context);
 
     auto sender
         = EX::schedule(s)
