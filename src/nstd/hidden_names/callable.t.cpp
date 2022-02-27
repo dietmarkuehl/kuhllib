1dbb4cad5c8164719f481d93ddf1bfdb8f60a79c
Dietmar Kuehl <dietmar.kuehl@me.com>
2022-02-27 20:31:14 +0000

    added callable concept

---
+++ src/nstd/hidden_names/callable.t.cpp
@@ -0,0 +1,67 @@
+// nstd/hidden_names/callable.t.cpp                                   -*-C++-*-
+// ----------------------------------------------------------------------------
+//  Copyright (C) 2022 Dietmar Kuehl http://www.dietmar-kuehl.de
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify,
+//  merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include "nstd/hidden_names/callable.hpp"
+#include "kuhl/test.hpp"
+
+namespace test_declarations {}
+namespace TD = ::test_declarations;
+namespace HN = ::nstd::hidden_names;
+namespace KT = ::kuhl::test;
+
+// ----------------------------------------------------------------------------
+
+namespace test_declarations {
+    namespace {
+        struct non_function {};
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+static KT::testcase const tests[] = {
+    KT::expect_success("non-callables", []{
+            return not HN::callable<int>
+                && not HN::callable<TD::non_function>
+                ;
+        }),
+    KT::expect_success("functions", []{
+            return HN::callable<void()>
+                && HN::callable<void(int), int>
+                && HN::callable<void(int, int&), int, int&>
+                && not HN::callable<void(int, int&), int, int>
+                ;
+        }),
+    KT::expect_success("lambdas", []{
+            return HN::callable<decltype([]{})>
+                && not HN::callable<decltype([](int){})>
+                && HN::callable<decltype([](int){}), int>
+                && HN::callable<decltype([](int, int&){}), int, int&>
+                && not HN::callable<decltype([](int, int&){}), int, int>
+                ;
+        }),
+};
+
+static KT::add_tests suite("hidden_names::callable", ::tests);
