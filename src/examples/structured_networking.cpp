a81bc19ff80246e5e46eeaa34bb617f7a170b835
Dietmar Kuehl <dietmar.kuehl@me.com>
2022-04-03 14:15:27 +0100

    structured_networking step 4

--- src/examples/structured_networking.cpp
+++ src/examples/structured_networking.cpp
@@ -54,4 +54,8 @@ int main()
     std::cout << std::unitbuf;
     io_context    context;
     socket_acceptor server(endpoint(ip::address_v4::any(), 12345));
+
+    sender auto s = schedule(context.scheduler())
+        ;
+    (void)s;
 }
