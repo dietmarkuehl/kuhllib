08b1932e2b3cbf126a363f9e12e72252f82530c4
Dietmar Kuehl <dietmar.kuehl@me.com>
2022-04-06 03:50:57 +0100

    fixed a silly perl issue

--- presentations/structured-networking/switch.pl
+++ presentations/structured-networking/switch.pl
@@ -2,7 +2,7 @@
 use strict;
 
 my $name = "structured-networking";
-my $gitlog = "$(name)-commits";
+my $gitlog = "$name-commits";
 open(GITLOG, "< $gitlog") || die("no $gitlog dump");
 my $commit = "";
 my $last   = 0;
