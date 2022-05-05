c0f9581fdb9247ddf1d789d0366e9fa89df4ad93
Dietmar Kuehl <dietmar.kuehl@me.com>
2022-05-05 22:42:34 +0100

    started implementing run_loop

---
+++ src/nstd/execution/run_loop.hpp
@@ -0,0 +1,119 @@
+// nstd/execution/run_loop.hpp                                        -*-C++-*-
+// ----------------------------------------------------------------------------
+//  Copyright (C) 2022 Dietmar Kuehl http://www.dietmar-kuehl.de
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify,
+//  merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#ifndef INCLUDED_NSTD_EXECUTION_RUN_LOOP
+#define INCLUDED_NSTD_EXECUTION_RUN_LOOP
+
+#include "nstd/execution/completion_signatures.hpp"
+#include "nstd/execution/connect.hpp"
+#include "nstd/execution/get_completion_scheduler.hpp"
+#include "nstd/execution/receiver.hpp"
+#include "nstd/execution/schedule.hpp"
+#include "nstd/execution/scheduler.hpp"
+#include "nstd/execution/sender.hpp"
+#include "nstd/execution/set_value.hpp"
+
+// ----------------------------------------------------------------------------
+
+namespace nstd::execution {
+    class run_loop;
+}
+
+namespace nstd::hidden_names::run_loop {
+    class state_base;
+    template <::nstd::execution::receiver> class state;
+    class sender;
+    class scheduler;
+    auto tag_invoke(::nstd::execution::get_completion_scheduler_t<::nstd::execution::set_value_t>,
+                    ::nstd::hidden_names::run_loop::sender const&)
+        -> ::nstd::hidden_names::run_loop::scheduler;
+}
+
+// ----------------------------------------------------------------------------
+
+class nstd::execution::run_loop
+{
+public:
+    using state_base = ::nstd::hidden_names::run_loop::state_base;
+    template <::nstd::execution::receiver Receiver>
+    using state = ::nstd::hidden_names::run_loop::state<Receiver>; //-dk:TODO user receiver_of<???>
+    using scheduler = ::nstd::hidden_names::run_loop::scheduler;
+    using sender = ::nstd::hidden_names::run_loop::sender;
+
+    run_loop() noexcept = default;
+    run_loop(run_loop&&) = delete;
+    ~run_loop() = default;
+
+    auto get_scheduler() -> scheduler;
+    auto run() -> void;
+    auto finish() -> void;
+};
+
+// ----------------------------------------------------------------------------
+
+class nstd::hidden_names::run_loop::sender
+{
+private:
+    ::nstd::execution::run_loop* d_loop;
+
+    auto get_completion_scheduler() const -> ::nstd::hidden_names::run_loop::scheduler;
+
+public:
+    using completion_signatures = ::nstd::execution::completion_signatures<
+            ::nstd::execution::set_value_t()
+        >;
+
+    friend auto tag_invoke(::nstd::execution::get_completion_scheduler_t<::nstd::execution::set_value_t>,
+                           ::nstd::hidden_names::run_loop::sender const&)
+        -> ::nstd::execution::run_loop::scheduler;
+    template <::nstd::execution::receiver Receiver>
+    friend auto tag_invoke(::nstd::execution::connect_t, sender&&, Receiver&&)
+        -> ::nstd::execution::run_loop::state<Receiver>;
+};
+
+static_assert(::nstd::execution::sender<::nstd::execution::run_loop::sender>);
+
+// ----------------------------------------------------------------------------
+
+class nstd::hidden_names::run_loop::scheduler
+{
+private:
+    friend class ::nstd::execution::run_loop;
+    friend class ::nstd::hidden_names::run_loop::sender;
+
+    ::nstd::execution::run_loop* d_loop;
+
+    scheduler(::nstd::execution::run_loop*);
+public:
+    auto operator== (scheduler const&) const -> bool = default;
+    friend auto tag_invoke(::nstd::execution::schedule_t, scheduler const&) noexcept
+        -> ::nstd::execution::run_loop::sender;
+};
+
+static_assert(::nstd::execution::scheduler<::nstd::execution::run_loop::scheduler>);
+
+// ----------------------------------------------------------------------------
+
+#endif
