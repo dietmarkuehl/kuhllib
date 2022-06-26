bf8ef272c5814fc4a74438e400d172559d598d88
Dietmar Kühl <dietmar.kuehl@me.com>
2022-06-27 00:12:02 +0100

    started work on updating let_*

--- src/nstd/execution/let.cpp
+++ src/nstd/execution/let.cpp
@@ -1,4 +1,4 @@
-// nstd/execution/let_value.cpp                                       -*-C++-*-
+// nstd/execution/let.cpp                                             -*-C++-*-
 // ----------------------------------------------------------------------------
 //  Copyright (C) 2021 Dietmar Kuehl http://www.dietmar-kuehl.de         
 //                                                                       
@@ -23,10 +23,10 @@
 //  OTHER DEALINGS IN THE SOFTWARE. 
 // ----------------------------------------------------------------------------
 
-#include "nstd/execution/let_value.hpp"
+#include "nstd/execution/let.hpp"
 
 // ----------------------------------------------------------------------------
 
 namespace nstd::execution {
-    int let_value_dummy = 0;
+    int let_dummy = 0;
 }
