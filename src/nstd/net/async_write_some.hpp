31f187590fc369308fcdb14bc2c6610b75958c3d
Dietmar Kühl <dietmar.kuehl@me.com>
2023-01-15 16:22:09 +0000

    moved async_write_some to the new async_io

--- src/nstd/net/async_write_some.hpp
+++ src/nstd/net/async_write_some.hpp
@@ -27,7 +27,9 @@
 #define INCLUDED_NSTD_NET_ASYNC_WRITE_SOME
 
 #include "nstd/file/async_io.hpp"
+#include "nstd/file/io_object.hpp"
 #include "nstd/net/io_context.hpp"
+#include "nstd/net/socket.hpp"
 #include "nstd/net/get_iovec.hpp"
 #include "nstd/net/socket.hpp"
 #include "nstd/execution/completion_signatures.hpp"
@@ -40,123 +42,66 @@
 
 // ----------------------------------------------------------------------------
 
-namespace nstd::net::hidden_names::async_write_some {
-    template <typename Socket, typename CBS> struct socket_operation;
-    template <typename Stream, typename CBS> struct stream_operation;
-    struct cpo;
+namespace nstd::hidden_names::async_write_some {
+    template <::nstd::net::socket Socket, typename Env>
+    struct operation {
+        using completion_signature = ::nstd::execution::set_value_t(::std::size_t);
+        
+        template <typename IOVector>
+        struct state {
+            IOVector           d_iovec;
+            ::msghdr           d_msg{};
+
+            template <typename Buffers>
+            state(Env const& env, Buffers&& buffers)
+                : d_iovec(::nstd::net::get_iovec(env, ::nstd::utility::forward<Buffers>(buffers)))
+            {
+                this->d_msg.msg_iov     = this->d_iovec.data();
+                this->d_msg.msg_iovlen  = this->d_iovec.size();
+            }
+            auto start(
+                ::nstd::net::socket auto& socket,
+                auto&& scheduler,
+                ::nstd::file::io_base* cont) noexcept -> void
+            {
+                scheduler.sendmsg(socket.native_handle(), &this->d_msg, int(), cont);
+            }
+            auto start(
+                ::nstd::file::io_object auto& object,
+                auto&& scheduler,
+                ::nstd::file::io_base* cont) noexcept -> void
+            {
+                scheduler.write(object.native_handle(), &this->d_iovec.data(), this->d_iovec.size(), cont);
+            }
+            template <typename Receiver>
+            auto complete(::std::int32_t n, ::std::uint32_t, Receiver& receiver) noexcept -> void {
+                ::nstd::execution::set_value(::nstd::utility::move(receiver), ::std::size_t(n));
+            }
+        };
+        template <typename Buffers>
+        static auto connect(Env const& env, Buffers&& buffers)
+        {
+            using iovec_t = decltype(::nstd::net::get_iovec(env, ::nstd::utility::forward<Buffers>(buffers)));
+            return state<iovec_t>(env, ::nstd::utility::forward<Buffers>(buffers));
+        }
+    };
 }
 
-// ----------------------------------------------------------------------------
-
-template <typename Socket, typename CBS>
-struct nstd::net::hidden_names::async_write_some::socket_operation {
-    using completion_signature = ::nstd::execution::set_value_t(int);
-    using buffer_sequence = ::nstd::type_traits::remove_cvref_t<CBS>;
-    template <typename Env>
-    using iovec = decltype(::nstd::net::get_iovec(::nstd::type_traits::declval<Env>(), ::nstd::type_traits::declval<buffer_sequence>()));
-
-    typename Socket::native_handle_type  d_handle;
-    CBS                                  d_buffer;
-    template <typename Env>
-    struct state {
-        iovec<Env> d_iovec;
-        ::msghdr   d_msg{};
-        state(Env const& env, buffer_sequence const& buffer)
-            : d_iovec(::nstd::net::get_iovec(env, buffer)) {
-            this->d_msg.msg_iov    = this->d_iovec.data();
-            this->d_msg.msg_iovlen = this->d_iovec.size();
-        }
-    };
-    template <typename Env>
-    auto connect(Env const& env) -> state<Env> {
-        return state<Env>(env, this->d_buffer);
-    }
-    template <typename Env>
-    auto start(::nstd::net::io_context::scheduler_type scheduler, state<Env>& s, ::nstd::file::context::io_base* cont) -> void{
-        scheduler.sendmsg(this->d_handle, &s.d_msg, int(), cont);
-    }
-    template <::nstd::execution::receiver Receiver, typename Env>
-    auto complete(int32_t rc, uint32_t, bool cancelled, state<Env>&, Receiver& receiver) -> void {
-        if (cancelled) {
-            ::nstd::execution::set_stopped(::nstd::utility::move(receiver));
-        }
-        else if (rc < 0) {
-            ::nstd::execution::set_error(::nstd::utility::move(receiver), ::std::error_code(-rc, std::system_category()));
-        }
-        else {
-            ::nstd::execution::set_value(::nstd::utility::move(receiver), rc);
-        }
-    }
-};
-
-// ----------------------------------------------------------------------------
-
-template <typename Stream, typename CBS>
-struct nstd::net::hidden_names::async_write_some::stream_operation {
-    using completion_signature = ::nstd::execution::set_value_t(int);
-    using buffer_sequence = ::nstd::type_traits::remove_cvref_t<CBS>;
-    template <typename Env>
-    using iovec = decltype(::nstd::net::get_iovec(::nstd::type_traits::declval<Env>(), ::nstd::type_traits::declval<buffer_sequence>()));
-
-    typename Stream::native_handle_type  d_handle;
-    CBS                                  d_buffer;
-    template <typename Env>
-    struct state {
-        iovec<Env> d_iovec;
-        ::msghdr   d_msg{};
-        state(Env const& env, buffer_sequence const& buffer)
-            : d_iovec(::nstd::net::get_iovec(env, buffer)) {
-        }
-    };
-    template <typename Env>
-    auto connect(Env const& env) -> state<Env> {
-        return state<Env>(env, this->d_buffer);
-    }
-    template <typename Env>
-    auto start(::nstd::net::io_context::scheduler_type scheduler, state<Env>& s, ::nstd::file::context::io_base* cont) -> void{
-        scheduler.write(this->d_handle, s.d_iovec.data(), s.d_iovec.size(), cont);
-    }
-    template <::nstd::execution::receiver Receiver, typename Env>
-    auto complete(int32_t rc, uint32_t, bool cancelled, state<Env>&, Receiver& receiver) -> void {
-        if (cancelled) {
-            ::nstd::execution::set_stopped(::nstd::utility::move(receiver));
-        }
-        else if (rc < 0) {
-            ::nstd::execution::set_error(::nstd::utility::move(receiver), ::std::error_code(-rc, std::system_category()));
-        }
-        else {
-            ::nstd::execution::set_value(::nstd::utility::move(receiver), rc);
-        }
-    }
-};
-
-// ----------------------------------------------------------------------------
-
-struct nstd::net::hidden_names::async_write_some::cpo {
-    template <::nstd::net::socket Socket, typename CBS>
-    friend auto tag_invoke(cpo, Socket& socket, CBS const& buffer) {
-        return nstd::file::hidden_names::async_io_sender<::nstd::net::hidden_names::async_write_some::socket_operation<Socket, CBS>>(
-            socket.native_handle(), buffer
-            );
-    }
-    template <typename Stream, typename CBS>
-    friend auto tag_invoke(cpo, Stream& stream, CBS const& buffer) {
-        return nstd::file::hidden_names::async_io_sender<::nstd::net::hidden_names::async_write_some::stream_operation<Stream, CBS>>(
-            stream.native_handle(), buffer
-            );
-    }
-
-    template <typename Socket, typename CBS>
-    auto operator()(Socket& socket, CBS const& buffer) const {
-        return ::nstd::tag_invoke(*this, socket, buffer);
-    }
-};
-
-// ----------------------------------------------------------------------------
-
 namespace nstd::net::inline customization_points {
-    using async_write_some_t = ::nstd::net::hidden_names::async_write_some::cpo;
-    inline constexpr async_write_some_t async_write_some;
+    using async_write_some_t =
+        ::nstd::hidden_names::async_io::cpo<
+            ::nstd::hidden_names::async_write_some::operation
+        >;
+    inline constexpr async_write_some_t async_write_some_adapter{};
+
+    template <::nstd::net::socket Socket, typename Buffers>
+    inline auto async_write_some(
+        Socket&& socket,
+        Buffers&& buffers) {
+        return ::nstd::net::async_write_some_adapter(
+            ::nstd::execution::just(::nstd::utility::forward<Buffers>(buffers)),
+            socket);
+    }
 }
 
 // ----------------------------------------------------------------------------
