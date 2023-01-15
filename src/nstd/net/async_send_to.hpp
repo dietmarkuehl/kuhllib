88f505ee212d888de3dd54bbbbbadf29161534f1
Dietmar Kühl <dietmar.kuehl@me.com>
2023-01-15 16:01:47 +0000

    moved async_send_to to the new async_io

--- src/nstd/net/async_send_to.hpp
+++ src/nstd/net/async_send_to.hpp
@@ -28,6 +28,7 @@
 
 #include "nstd/file/async_io.hpp"
 #include "nstd/file/operation.hpp"
+#include "nstd/net/socket.hpp"
 #include "nstd/net/get_iovec.hpp"
 #include "nstd/hidden_names/message_flags.hpp"
 #include "nstd/execution/completion_signatures.hpp"
@@ -43,89 +44,82 @@
 
 // ----------------------------------------------------------------------------
 
-namespace nstd::net::hidden_names::async_send_to {
-    template <typename Socket, typename CB> struct operation;
-    struct cpo;
-}
+namespace nstd::hidden_names::async_send_to {
+    template <::nstd::net::socket Socket, typename Env>
+    struct operation {
+        using completion_signature = ::nstd::execution::set_value_t(::std::size_t);
+        
+        template <typename IOVector>
+        struct state {
+            IOVector           d_iovec;
+            ::sockaddr_storage d_address;
+            ::msghdr           d_msg{};
+            int                d_flags;
 
-// ----------------------------------------------------------------------------
-
-template <typename Socket, typename CB>
-struct nstd::net::hidden_names::async_send_to::operation {
-    using completion_signature = ::nstd::execution::set_value_t(int);
-    using buffer_sequence = ::nstd::type_traits::remove_cvref_t<CB>;
-    template <typename Env>
-    using iovec = decltype(::nstd::net::get_iovec(::nstd::type_traits::declval<Env>(), ::nstd::type_traits::declval<buffer_sequence>()));
-
-    typename Socket::native_handle_type      d_handle;
-    ::nstd::type_traits::remove_cvref_t<CB>  d_buffer;
-    ::nstd::hidden_names::message_flags      d_flags;
-    typename Socket::endpoint_type           d_endpoint;
-
-    template <typename Env>
-    struct state {
-        ::sockaddr_storage d_address;
-        iovec<Env> d_iovec;
-        ::msghdr   d_msg;
-        state(Env const& env, buffer_sequence const& buffer, typename Socket::endpoint_type& endpoint)
-            : d_iovec(::nstd::net::get_iovec(env, buffer)) {
-            this->d_msg.msg_name    = &this->d_address;
-            this->d_msg.msg_namelen = endpoint.get_address(&this->d_address);
-            this->d_msg.msg_iov     = this->d_iovec.data();
-            this->d_msg.msg_iovlen  = this->d_iovec.size();
+            template <typename Buffers>
+            state(Env const& env, Buffers&& buffers, ::nstd::hidden_names::message_flags flags, typename Socket::endpoint_type const& endpoint)
+                : d_iovec(::nstd::net::get_iovec(env, ::nstd::utility::forward<Buffers>(buffers)))
+                , d_flags(static_cast<int>(flags))
+            {
+                this->d_msg.msg_iov     = this->d_iovec.data();
+                this->d_msg.msg_iovlen  = this->d_iovec.size();
+                this->d_msg.msg_name    = &this->d_address;
+                this->d_msg.msg_namelen = endpoint.get_address(&this->d_address);
+            }
+            auto start(
+                Socket& socket,
+                auto&& scheduler,
+                ::nstd::file::io_base* cont) noexcept -> void
+            {
+                scheduler.sendmsg(socket.native_handle(), &this->d_msg, this->d_flags, cont);
+            }
+            template <typename Receiver>
+            auto complete(::std::int32_t n, ::std::uint32_t, Receiver& receiver) noexcept -> void {
+                ::nstd::execution::set_value(::nstd::utility::move(receiver), ::std::size_t(n));
+            }
+        };
+        template <typename Buffers>
+        static auto connect(Env const& env, Buffers&& buffers, typename Socket::endpoint_type const& endpoint)
+        {
+            using iovec_t = decltype(::nstd::net::get_iovec(env, ::nstd::utility::forward<Buffers>(buffers)));
+            return state<iovec_t>(env, ::nstd::utility::forward<Buffers>(buffers), ::nstd::hidden_names::message_flags{}, endpoint);
+        }
+        template <typename Buffers>
+        static auto connect(Env const& env, Buffers&& buffers, ::nstd::hidden_names::message_flags flags, typename Socket::endpoint_type const& endpoint)
+        {
+            using iovec_t = decltype(::nstd::net::get_iovec(env, ::nstd::utility::forward<Buffers>(buffers)));
+            return state<iovec_t>(env, ::nstd::utility::forward<Buffers>(buffers), flags, endpoint);
         }
     };
-    template <typename Env>
-    auto connect(Env const& env) -> state<Env> {
-        return state<Env>(env, this->d_buffer, this->d_endpoint);
-    }
-    template <typename Env>
-    auto start(::nstd::net::io_context::scheduler_type scheduler, state<Env>& s, ::nstd::file::context::io_base* cont) -> void{
-        scheduler.sendmsg(this->d_handle, &s.d_msg, static_cast<int>(this->d_flags), cont);
-    }
-    template <::nstd::execution::receiver Receiver, typename Env>
-    auto complete(int32_t rc, uint32_t, bool cancelled, state<Env>&, Receiver& receiver) -> void {
-        if (cancelled) {
-            ::nstd::execution::set_stopped(::nstd::utility::move(receiver));
-        }
-        else if (rc < 0) {
-            ::nstd::execution::set_error(::nstd::utility::move(receiver), ::std::error_code(-rc, std::system_category()));
-        }
-        else {
-            ::nstd::execution::set_value(::nstd::utility::move(receiver), rc);
-        }
-    }
-};
-
-// ----------------------------------------------------------------------------
-
-struct nstd::net::hidden_names::async_send_to::cpo {
-    template <typename Socket, typename CB>
-    friend auto tag_invoke(cpo, Socket& socket, CB&& cb, ::nstd::hidden_names::message_flags flags, typename Socket::endpoint_type endpoint) {
-        return nstd::file::hidden_names::async_io_sender<::nstd::net::hidden_names::async_send_to::operation<Socket, CB>>(
-            socket.native_handle(), ::nstd::utility::forward<CB>(cb), flags, endpoint
-            );
-    }
-    template <typename Socket, typename CB>
-    auto operator()(Socket& socket, CB&& cb, ::nstd::hidden_names::message_flags flags, typename Socket::endpoint_type endpoint) const {
-        return ::nstd::tag_invoke(*this, socket, ::nstd::utility::forward<CB>(cb), flags, endpoint);
-    }
-    template <typename Socket, typename CB>
-    auto operator()(Socket& socket, CB&& cb, typename Socket::endpoint_type endpoint) const {
-        return ::nstd::tag_invoke(*this, socket,
-                                  ::nstd::utility::forward<CB>(cb),
-                                  ::nstd::hidden_names::message_flags(),
-                                  endpoint);
-    }
-};
-
-// ----------------------------------------------------------------------------
-
-namespace nstd::net::inline customization_points {
-    using async_send_to_t = ::nstd::net::hidden_names::async_send_to::cpo;
-    inline constexpr async_send_to_t async_send_to;
 }
 
+namespace nstd::net::inline customization_points {
+    using async_send_to_t =
+        ::nstd::hidden_names::async_io::cpo<
+            ::nstd::hidden_names::async_send_to::operation
+        >;
+    inline constexpr async_send_to_t async_send_to_adapter{};
+
+    template <::nstd::net::socket Socket, typename Buffers>
+    inline auto async_send_to(
+        Socket&& socket,
+        Buffers&& buffers,
+        typename ::nstd::type_traits::remove_cvref_t<Socket>::endpoint_type const& endpoint)  {
+        return ::nstd::net::async_send_to_adapter(
+            ::nstd::execution::just(::nstd::utility::forward<Buffers>(buffers), endpoint),
+            socket);
+    }
+    template <::nstd::net::socket Socket, typename Buffers>
+    inline auto async_send_to(
+        Socket&& socket,
+        ::nstd::hidden_names::message_flags flags,
+        Buffers&& buffers,
+        typename ::nstd::type_traits::remove_cvref_t<Socket>::endpoint_type const& endpoint)  {
+        return ::nstd::net::async_send_to_adapter(
+            ::nstd::execution::just(::nstd::utility::forward<Buffers>(buffers), flags, endpoint),
+            socket);
+    }
+}
 
 // ----------------------------------------------------------------------------
 
