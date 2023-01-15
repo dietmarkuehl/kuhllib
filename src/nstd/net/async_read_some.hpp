b2b34d718623650ba043c96f5857816a1cc01a40
Dietmar Kühl <dietmar.kuehl@me.com>
2023-01-15 15:36:00 +0000

    moved async_receive to the new async_io

--- src/nstd/net/async_read_some.hpp
+++ src/nstd/net/async_read_some.hpp
@@ -50,7 +50,7 @@ namespace nstd::hidden_names::async_read_some {
         template <typename IOVector>
         struct state {
             IOVector d_iovec;
-            ::msghdr d_msg{};
+            ::msghdr d_msg{}; //-dk:TODO don't build a msghdr for non-sockets
 
             template <typename Buffers>
             state(Env const& env, Buffers&& buffers)
@@ -95,113 +95,12 @@ namespace nstd::net::inline customization_points {
     inline constexpr async_read_some_t async_read_some_adapter{};
 
     template <::nstd::file::io_object IOObject, typename Buffers>
-    inline auto async_read_some( IOObject&& io_object, Buffers&& buffers)  {
+    inline auto async_read_some(IOObject&& io_object, Buffers&& buffers)  {
         return ::nstd::net::async_read_some_adapter(
             ::nstd::execution::just(::nstd::utility::forward<Buffers>(buffers)),
             io_object);
     }
 }
-
-#if 0
-namespace nstd::net::hidden_names::async_read_some {
-    template <typename Socket, typename MBS> struct socket_operation;
-    template <typename Socket, typename MBS> struct stream_operation;
-    struct cpo;
-}
-
-// ----------------------------------------------------------------------------
-
-template <typename Socket, typename MBS>
-struct nstd::net::hidden_names::async_read_some::socket_operation {
-    using completion_signature = ::nstd::execution::set_value_t(int);
-    using buffer_sequence = ::nstd::type_traits::remove_cvref_t<MBS>;
-    template <typename Env>
-    using iovec = decltype(::nstd::net::get_iovec(::nstd::type_traits::declval<Env>(), ::nstd::type_traits::declval<buffer_sequence>()));
-
-    typename Socket::native_handle_type  d_handle;
-    buffer_sequence                      d_buffer;
-    template <typename Env>
-    struct state {
-        iovec<Env> d_iovec;
-        ::msghdr   d_msg{};
-        state(Env const& env, buffer_sequence const& buffer)
-            : d_iovec(::nstd::net::get_iovec(env, buffer)) {
-            this->d_msg.msg_iov    = this->d_iovec.data();
-            this->d_msg.msg_iovlen = this->d_iovec.size();
-        }
-    };
-    template <typename Env>
-    auto connect(Env const& env) -> state<Env> {
-        return state<Env>(env, this->d_buffer);
-    }
-    template <typename Env>
-    auto start(::nstd::net::io_context::scheduler_type scheduler, state<Env>& s, ::nstd::file::context::io_base* cont) -> void{
-        scheduler.recvmsg(this->d_handle, &s.d_msg, int(), cont);
-    }
-    template <::nstd::execution::receiver Receiver, typename Env>
-    auto complete(int32_t rc, uint32_t, bool cancelled, state<Env>&, Receiver& receiver) -> void {
-        if (cancelled) {
-            ::nstd::execution::set_stopped(::nstd::utility::move(receiver));
-        }
-        else if (rc < 0) {
-            ::nstd::execution::set_error(::nstd::utility::move(receiver), ::std::error_code(-rc, std::system_category()));
-        }
-        else {
-            ::nstd::execution::set_value(::nstd::utility::move(receiver), rc);
-        }
-    }
-};
-
-// ----------------------------------------------------------------------------
-
-template <typename Stream, typename MBS>
-struct nstd::net::hidden_names::async_read_some::stream_operation {
-    using completion_signature = ::nstd::execution::set_value_t(int);
-    using buffer_sequence = ::nstd::type_traits::remove_cvref_t<MBS>;
-    template <typename Env>
-    using iovec = decltype(::nstd::net::get_iovec(::nstd::type_traits::declval<Env>(), ::nstd::type_traits::declval<buffer_sequence>()));
-
-    typename Stream::native_handle_type d_handle;
-    buffer_sequence                     d_buffer;
-
-    template <typename Env>
-    struct state {
-        iovec<Env> d_iovec;
-    };
-    template <typename Env>
-    auto connect(Env const& env) -> state<Env> {
-        return { ::nstd::net::get_iovec(env, this->d_buffer) };
-    }
-    template <typename Env>
-    auto start(::nstd::net::io_context::scheduler_type scheduler, state<Env>& s, ::nstd::file::context::io_base* cont) -> void{
-        scheduler.read(this->d_handle, s.d_iovec.data(), s.d_iovec.size(), cont);
-    }
-    template <::nstd::execution::receiver Receiver, typename Env>
-    auto complete(int32_t rc, uint32_t, bool cancelled, state<Env>&, Receiver& receiver) -> void {
-        if (cancelled) {
-            ::nstd::execution::set_stopped(::nstd::utility::move(receiver));
-        }
-        else if (rc < 0) {
-            ::nstd::execution::set_error(::nstd::utility::move(receiver), ::std::error_code(-rc, std::system_category()));
-        }
-        else {
-            ::nstd::execution::set_value(::nstd::utility::move(receiver), rc);
-        }
-    }
-};
-
-// ----------------------------------------------------------------------------
-
-struct nstd::net::hidden_names::async_read_some::cpo {
-    template <::nstd::net::socket Socket, typename MBS>
-    friend auto tag_invoke(cpo, Socket& socket, MBS const& buffer) {
-        return nstd::file::hidden_names::async_io_sender<::nstd::net::hidden_names::async_read_some::socket_operation<Socket, MBS>>(
-            socket.native_handle(), buffer
-            );
-    }
-};
-
-#endif
 
 // ----------------------------------------------------------------------------
 
