d7dfc62973da5e8288e7cfa41d19f2db51526e26
Dietmar Kühl <dietmar.kuehl@me.com>
2023-01-16 22:04:55 +0000

    added a section introducing schedulers

--- src/tutorial/README.md
+++ src/tutorial/README.md
@@ -82,3 +82,81 @@ to show a rather basic sender/receiver example. The benefit of these
 abstractions start working out when some of the operations can be
 asynchronous and are composed into workflows by algorithms operating
 on the sender/receiver abstraction.
+
+## Chaining Work on the Completion of Other Sender: `then`
+
+## Asynchronously Scheduling Work: Schedulers
+
+So far the examples were just a fairly complicated way of executing
+work sequentially. To actually manage asynchronous work _schedulers_
+are used. A scheduler provides a `schedule` operation which provides
+a sender. After the sender is `connect()`ed and the operation state
+got `start()`, the sender completes on on the scheduler's computation
+resource. When that happens is controlled by the scheduler.
+
+Schedulers are copyable and are, thus, typically lightweight handles
+to some execution context. The execution context may manage an event
+queue from a UI, outstanding I/O operations when dealing with
+external source, a thread pool for work, the necessary resources
+to off-load work to a GPU, etc.
+
+The [manual scheduling example](getting-started-3.cpp) demonstrates
+the operation of scheduler using a context managing a stack of
+outstanding work:
+
+- The class `manual_context` uses a singly linked, intrusive list
+    of work items. The work `submit()`ed to the context is represented
+    by a pointer to a `state_base` which holds a pointer `d_next`
+    to the next work item or `nullptr` if there is no more work.
+    The downstream work is scheduled by `complete()`ing the
+    `schedule()` sender's operation state.
+
+    The public operation of the `manual_context` are:
+
+    1. An `empty()` function indicating that there is no outstanding
+        work.
+    2. A `get_scheduler()` function yielding an object of type
+        `manual_context::scheduler` which is used to schedule work
+        on the context.
+    3. A function `run_next()` is running the next scheduled work
+        item by completing the sender on the current thread.
+
+- The `manual_context::scheduler` returned from `get_scheduler()`
+    supports just one operation: `schedule()`. The main functionality
+    of the scheduler abstraction to clearly indicate what operations
+    are run where. Also, unlike senders, schedulers are always
+    copyable.
+
+    The `schedule()` operation yields a `manual_context::sender`
+    used to chain work on the completion of the scheduling operation.
+
+- The `manual_context::sender` declares that it doesn't produce any
+    useful result by defining `result_t` to be `tut::none`. When
+    the model gets refined sender not yielding a value will eventually
+    just call `set_value()` without any arguments but for now having
+    `set_value` always yield exactly one value is a huge simplification.
+
+    As all senders, `manual_context::sender` provides a `connect()`
+    function, yielding a `manual_context::state<Receiver>.
+
+- The `manual_context::state<Receiver>`'s main operation is `start()`
+    which `submit()`s work with the context. As the `state` object is
+    the actual work, it inherits from `sender_base` and overrides the
+    `virtual` function `complete()`. The `complete()` function just
+    calls `set_value(tut::none{})` on the `connect()`ed receiver.
+
+    One important aspect of the operation states is that it can't
+    be copied or moved: this way the `state_base` can be pointed
+    to directly and an intrusive list can be used. This technique
+    is quite common to avoid allocations. As operation states are
+    returned from `connect()` and won't have copy or move constructor
+    the entirely model heavely depends on [mandatory copy
+    elision](https://en.cppreference.com/w/cpp/language/copy_elision).
+
+The `main()` function of this example exercises the scheduling
+operation of the `manual_context`: it `schedule()`s and `connect()`s
+two piece of work using `then()` senders from the previous section.
+Each corresponding state object gets `start()`ed which, of course,
+just registered the work to be done with the scheduler. Once the
+work is submitted the loop at the end just executes all work of the
+`context`.
---
+++ src/tutorial/getting-started-3.cpp
@@ -0,0 +1,163 @@
+// getting-started-3.cpp                                              -*-C++-*-
+// ----------------------------------------------------------------------------
+//  Copyright (C) 2023 Dietmar Kuehl http://www.dietmar-kuehl.de
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify,
+//  merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include <iostream>
+#include <utility>
+#include <cassert>
+
+// ----------------------------------------------------------------------------
+// just - a sender producing a value
+
+namespace tut {
+    template <typename Value>
+    class just {
+    private:
+        template <typename Receiver>
+        struct state {
+            Receiver d_receiver;
+            Value    d_value;
+
+            void start() { d_receiver.set_value(d_value); }
+        };
+
+        Value d_value;
+    public:
+        using result_t = Value;
+
+        just(Value const& value): d_value(value) {}
+        template <typename Receiver>
+        auto connect(Receiver receiver) {
+            return state<Receiver>{ receiver, d_value };
+        }
+    };
+}
+
+// ----------------------------------------------------------------------------
+// A context with a scheduler to run work items manually controlled: each
+// time run_next() is called, the next work item is extracted and run.
+
+namespace tut {
+    struct none { // type returned when there is no useful result
+        friend std::ostream& operator<< (std::ostream& out, none) {
+            return out << "<none>";
+        }
+    };
+
+    class manual_context {
+    public:
+        class scheduler;
+    
+    private:
+        struct state_base {
+            state_base() = default;
+            state_base(state_base&&) = delete;
+            state_base* d_next{nullptr};
+            virtual void complete() = 0;
+        };
+        template <typename Receiver>
+        struct state
+            : state_base
+        {
+            manual_context* d_context;
+            Receiver        d_receiver;
+
+            state(manual_context* context, Receiver receiver)
+                : d_context(context)
+                , d_receiver(receiver) {
+            }
+            void start() {
+                std::cout << "state::start: " << this << "\n";
+                d_context->submit(this);
+            }
+            void complete() override {
+                std::cout << "state::complete: " << this << "\n";
+                d_receiver.set_value(tut::none{});
+            }
+        };
+
+        class sender {
+            friend class scheduler;
+            manual_context* d_context;
+
+        public:
+            using result_t = tut::none;
+            explicit sender(manual_context* context): d_context(context) {}
+            template <typename Receiver>
+            state<Receiver> connect(Receiver receiver){ return state<Receiver>(d_context, receiver); }
+        };
+    
+        state_base* d_stack{nullptr};
+        void submit(state_base* work) {
+            work->d_next = std::exchange(d_stack, work);
+            std::cout << "submit: " << work << " => stack=" << d_stack << " next=" << work->d_next << " empty()=" << std::boolalpha << empty() << "\n";
+        }
+    public:
+        class scheduler {
+            friend class manual_context;
+            manual_context* d_context;
+
+        public:
+            explicit scheduler(manual_context* context): d_context(context) {}
+            sender schedule() { return sender(this->d_context); }
+        };
+
+        scheduler get_scheduler() { return scheduler(this); }
+        bool      empty() const { return d_stack == nullptr; }
+        void      run_next() {
+            assert(!empty());
+            std::exchange(d_stack, d_stack->d_next)->complete();
+        }
+    };
+}
+
+// ----------------------------------------------------------------------------
+
+namespace {
+    struct receiver {
+        template <typename T>
+        void set_value(T const& value) {
+            std::cout << "receiver::set_value(" << value << ")\n";
+        }
+    };
+}
+
+// ----------------------------------------------------------------------------
+
+int main()
+{
+    tut::manual_context context;
+
+    auto state1(context.get_scheduler().schedule().connect(receiver()));
+    state1.start();
+    auto state2(context.get_scheduler().schedule().connect(receiver()));
+    state2.start();
+
+    std::cout << "two state objects are started\n";
+
+    while (!context.empty()) {
+        std::cout << "running next\n";
+        context.run_next();
+    }
+}
